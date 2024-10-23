d4f92571714d1a220ce8e0ffdcae39ffb80e015b
dongyinuo <hndyn410329arno@gmail.com>
2024-10-23 18:40:44 +0800

    init

---
+++ src/test/java/com/example/springbootliquibaseexample/SpringbootLiquibaseExampleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springbootliquibaseexample;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLiquibaseExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
